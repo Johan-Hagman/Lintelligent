9b5d1337bdaf448b81076c369754ce16ab6f3176
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-10-14 15:51:05 +0200

    Add ts to project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import "dotenv/config";
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT) || 3001;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (_req: Request, res: Response) => {
   res.json({ message: "API Server is running!" });
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`API Server running on port ${PORT}`);
 });
